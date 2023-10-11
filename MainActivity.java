1f3468db67ca08c0febb11b8ce3ae6635c3997f0
Moditha Dissanayaka <108186528+Mo-Ditha@users.noreply.github.com>
2023-10-11 21:14:12 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.studentregisterwithfirbase;
+
+
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //set the title
+        // Check if the ActionBar exists before trying to modify it
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Student Register App"); // Set the title of the ActionBar
+        }
+
+
+
+        //open Login Activity
+        Button buttonLogin = findViewById(R.id.button_login);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //Open Register Activity
+        Button buttonRegister = findViewById(R.id.button_register);
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,StudentRegister.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
